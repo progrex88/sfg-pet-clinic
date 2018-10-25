3084b122e7213fe855383e874bfe7df7604375b7
progrex <progrex88@gmail.com>
2018-10-25 19:29:38 +0200

    Adding Owners Index Page and Controller. Closes #13

---
+++ pet-clinic-web/src/main/java/pl/com/progrex88/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,16 @@
+package pl.com.progrex88.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"","/","/index","/index.html"})
+    private String listOwners(){
+
+        return "owners/index";
+    }
+
+}
