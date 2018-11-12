f7f54fbea8eacbe8a27482518f4f696d35cd5259
progrex <progrex88@gmail.com>
2018-11-12 10:08:58 +0100

    Add Owners List and update controller and fixed mapping name. Closes #16

--- pet-clinic-web/src/main/java/pl/com/progrex88/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/com/progrex88/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package pl.com.progrex88.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.com.progrex88.sfgpetclinic.services.OwnerService;
 
-@RequestMapping("/owenrs")
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+       private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    private String listOwners(){
+    private String listOwners(Model model){
+model.addAttribute("owners",ownerService.findAll());
 
         return "owners/index";
     }
