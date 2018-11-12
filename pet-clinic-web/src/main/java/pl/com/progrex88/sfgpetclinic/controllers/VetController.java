57dd596c67a8853680724759b68ffd170e4481ed
progrex <progrex88@gmail.com>
2018-11-12 10:21:09 +0100

    Add Owners Vets and update controller and change mapping style. Closes #15

--- pet-clinic-web/src/main/java/pl/com/progrex88/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/com/progrex88/sfgpetclinic/controllers/VetController.java
@@ -2,13 +2,24 @@ package pl.com.progrex88.sfgpetclinic.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.com.progrex88.sfgpetclinic.services.VetService;
 
 @Controller
+@RequestMapping("/vets")
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
