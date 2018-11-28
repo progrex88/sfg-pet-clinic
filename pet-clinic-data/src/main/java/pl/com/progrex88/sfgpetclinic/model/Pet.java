e5c93f3a93531c56922fa600fb9defbf4ad8e7b7
progrex <progrex88@gmail.com>
2018-11-28 21:29:08 +0100

    added visits, update owner with pet relationship. Closes #26

--- pet-clinic-data/src/main/java/pl/com/progrex88/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/pl/com/progrex88/sfgpetclinic/model/Pet.java
@@ -1,18 +1,36 @@
 package pl.com.progrex88.sfgpetclinic.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.time.LocalDate;
 
 
 public class Pet extends BaseEntity {
 
-    @Getter
-    @Setter
+
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
 
+    public PetType getPetType() {
+        return petType;
+    }
 
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
 }
--- pet-clinic-data/src/main/java/pl/com/progrex88/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/pl/com/progrex88/sfgpetclinic/model/PetType.java
@@ -1,14 +1,16 @@
 package pl.com.progrex88.sfgpetclinic.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-
 public class PetType extends BaseEntity{
 
 
-    @Getter
-    @Setter
+
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
